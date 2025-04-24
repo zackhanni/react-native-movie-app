fa04f52f5334a48df4192ec2c4bcdd71aaf1d329
Zack Hanni <zack.hannI@gmail.com>
2025-04-24 10:58:13 -0400

    created basic tabs navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,8 +5,6 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
-      <Link href={"/onboarding"}>Onboarding</Link>
-      <Link href={"/movie/avengers"}>Avengers Movie</Link>
     </View>
   );
 }
---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MovieDetails = () => {
+  return (
+    <View>
+      <Text>Movie Details</Text>
+    </View>
+  );
+};
+
+export default MovieDetails;
