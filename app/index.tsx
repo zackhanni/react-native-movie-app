7c2c49cc70d5e3fb4c6d2e8e70b19f9f5818bd3d
Zack Hanni <zack.hannI@gmail.com>
2025-04-24 10:39:03 -0400

    tested routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Soma</Text>
+      <Text className="text-5xl text-dark-200 font-bold">Welcome!</Text>
+      <Link href={"/onboarding"}>Onboarding</Link>
+      <Link href={"/movie/avengers"}>Avengers Movie</Link>
     </View>
   );
 }
